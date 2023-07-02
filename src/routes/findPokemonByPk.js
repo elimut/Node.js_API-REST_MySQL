41504c6d8beed17ac8e121db1903fd455b68c57f
elimut <vandingenenannesophie@gmail.com>
2023-07-02 15:44:37 +0200

    findByPk

--- src/routes/findPokemonByPk.js
+++ src/routes/findPokemonByPk.js
@@ -3,6 +3,7 @@ const { Pokemon } = require('../db/sequelize')
 module.exports = (app) => {
   app.get('/api/pokemons/:id', (req, res) => {
     Pokemon.findByPk(req.params.id)
+    // pas de parseInt car findByPk le fait d'elle même
       .then(pokemon => {
         const message = 'Un pokémon a bien été trouvé.'
         res.json({ message, data: pokemon })
